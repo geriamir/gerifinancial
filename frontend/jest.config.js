b2d5b01936d202fb1e20e0eafeae280689ea2722
Amir Geri <amgeri@microsoft.com>
2025-06-22 15:41:43 +0300

    Update Jest configuration with static asset mocks

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,20 +1,19 @@
 module.exports = {
-  preset: 'react-scripts',
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { configFile: './babel.config.js' }],
-  },
-  // Make sure Jest can find node_modules
-  moduleDirectories: ['node_modules', '<rootDir>/src'],
-
-  // Add test setup file
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-
-  // Test environment setup
+  roots: ['<rootDir>/src'],
   testEnvironment: 'jsdom',
-
-  // Include TypeScript files
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/src/test/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/test/__mocks__/fileMock.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[tj]sx?$',
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest'
+  },
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js,jsx}',
+    '!src/**/*.d.ts'
   ],
 };
